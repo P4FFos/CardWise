24f332da9ef7fa94e8788e4e55d2ef1ccdcd6276
Danis Music <danism@student.chalmers.se>
2024-09-14 17:55:38 +0200

    Issue#4 Edit attributes in deck schema

--- server/models/deck.js
+++ server/models/deck.js
@@ -1,11 +1,15 @@
 var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
-mongoose.connect("'mongodb://localhost:27017/animalDevelopmentDB");
+mongoose.connect("mongodb://localhost:27017/animalDevelopmentDB");
 
 var deckSchema = new Schema({
     name: {type: String },
-    cards: {type: Array }
+    cards: [
+        {
+            content: {type: String, required: false},
+            explanation: {type: String, required: false}
+        }]
 });
 
 module.exports = mongoose.model('decks', deckSchema);
