480c8c1555bdea63be774bfbfbc48db9c1b60f7e
Danis Music <danism@student.chalmers.se>
2024-09-16 15:19:17 +0200

    Issue#5 Add derived attribute cardAmount to deck schema

--- server/models/deck.js
+++ server/models/deck.js
@@ -6,5 +6,15 @@ var deckSchema = new Schema({
     cards: [{type: Schema.Types.ObjectId, ref: "card"}]
 });
 
+
+// Create derived attribute cardAmount
+deckSchema.virtual('cardAmount').get(function() {
+    return this.cards ? this.cards.length: 0;
+});
+
+// Make it visible in using GET
+deckSchema.set('toJSON', { virtuals: true });
+deckSchema.set('toObject', { virtuals: true });
+
 module.exports = mongoose.model('deck', deckSchema);
 
