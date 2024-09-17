4bcedaf54b2d3ee77d16693e42903108e301387a
mykhailo <mykhailo@student.chalmers.se>
2024-09-17 22:28:25 +0200

    Issue#6 add basic handler methods for achievements

--- server/controllers/achievement.js
+++ server/controllers/achievement.js
@@ -1,8 +1,78 @@
 var express = require('express');
 var router = express.Router();
+var app = express();
+
+app.use(express.json());
 
 var Achievement = require('../models/achievement.js');
 
-//TODO: add endpoints from req list for achievements
+// Create a new achievement
+router.post('/api/achievements', async function(req, res, next) {
+    var achievement = new Achievement(req.body);
+    try {
+        await achievement.save();
+        if (achievement == null) {
+            res.status(404).json({"message": "Cannot create a null achievement."})
+        }
+    } catch (error) {
+        return next(error);
+    }
+    res.status(201).json(achievement);    
+});
+
+// Info of all achievements
+router.get('/api/achievements', async function(req, res, next) {
+    var achievements;
+    try {
+        achievements = await Achievement.find();
+    } catch (error) {
+        return next(error);
+    }
+    res.json({"achievements": achievements});
+});
+
+// Restarting progress through deleting achievements 
+router.delete('/api/achievements', async function(req, res, next) {
+    try {
+        const result = await Achievement.deleteMany({});
+        res.status(200).json({
+            message: result.deletedCount + " " + "Achievement(s) deleted. Progress reastarted.",
+            result: result
+        });
+    } catch (error) {
+        return next(error);
+    }
+});
+
+// Get info from a spec achievement
+router.get('/api/achievements/:id', async function(req, res, next) {
+    try {
+        const achievement = await Achievement.findById(req.params.id);
+        if (achievement == null) {
+            return res.status(404).json({"message": "Achievement with given id cannot be found."});
+        }
+        res.json(achievement);
+    } catch (error) {
+        return next(error);
+    }
+})
+
+// Put info into spec achievement
+router.put('/api/achievements/:id', async function(req, res, next) {
+    try {
+        const achievement = await Achievement.findById(req.params.id);
+        if (achievement == null) {
+            return res.status(404).json({"message": "Achievement not found"});
+        }
+        achievement.name = req.body.name;
+        achievement.type = req.body.type;
+
+        await achievement.save();
+        res.json(achievement);
+    } catch (err) { 
+        return next(err); 
+    }
+});
+
 
 module.exports = router;
