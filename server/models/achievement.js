3c91085f2888105d4df9b5877ab6a580e7de6d75
mykhailo <mykhailo@student.chalmers.se>
2024-09-17 22:28:25 +0200

    Issue#6 add basic handler methods for achievements

--- server/models/achievement.js
+++ server/models/achievement.js
@@ -4,7 +4,6 @@ var Schema = mongoose.Schema;
 var achievementSchema = new Schema({
   name: { type: String },
   type: { type: String },
-  id: { type: String },
 });
 
 module.exports = mongoose.model('achievement', achievementSchema);
