5ad410214ccb5b592912b682952109460f889409
mykhailo <mykhailo@student.chalmers.se>
2024-09-18 22:55:56 +0200

    Issue#6 fix routing and achievemnts models

--- server/models/streak_achievement.js
+++ server/models/streak_achievement.js
@@ -1,3 +1,8 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+var Achievement = require('./achievement.js');
+
 const streakAchievementSchema = new Schema({
   streakCounter: { type: Number, required: true }
 });
