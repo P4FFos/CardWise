36b04b251000f5afcf49588570b2aaa3740d1773
Danylo <gusbaranda@student.gu.se>
2024-09-29 13:44:21 +0200

    Issue#14 Implement registration page

--- client/src/router.js
+++ client/src/router.js
@@ -1,9 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
-
 import Home from './views/Home.vue'
+import Registration from './views/Registration.vue'
 
 const routes = [
-  { path: '/', name: 'home', component: Home }
+  { path: '/', name: 'home', component: Home },
+  { path: '/registration', name: 'registration', component: Registration }
 ]
 
 const router = createRouter({
--- client/src/views/Registration.vue
+++ client/src/views/Registration.vue
@@ -1,24 +1,70 @@
 <template>
   <div>
-    <h1>Register Page</h1>
-    <form>
+    <h1>Registration Page</h1>
+    <form @submit.prevent="register">
       <label for="username">Username:</label>
-      <input type="text" id="username" name="username">
+      <input type="text" id="username" v-model="username" name="username" required>
+
+      <label for="email">Email:</label>
+      <input type="email" id="email" v-model="email" name="email" required>
 
       <label for="password">Password:</label>
-      <input type="password" id="password" name="password">
+      <input type="password" id="password" v-model="password" name="password" required>
 
       <button type="submit">Register</button>
     </form>
+    <button @click="showUserInfo">Show User Info</button>
+    <div v-if="userInfo">
+      <p>User Information</p>
+      <p>Username: {{ userInfo.username }}</p>
+      <p>Email: {{ userInfo.email }}</p>
+    </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
+// specify the base URL for the API server
+axios.defaults.baseURL = 'http://localhost:3000'
+
 export default {
-  name: 'Register'
+  name: 'Registration',
+  data() {
+    return {
+      username: '',
+      email: '',
+      password: '',
+      userInfo: null,
+      userId: ''
+    }
+  },
+  methods: {
+    async register() {
+      try {
+        const response = await axios.post('/api/v1/users', {
+          username: this.username,
+          email: this.email,
+          password: this.password
+        })
+        this.userId = response.data.user._id
+        alert('User successfully registered')
+      } catch (error) {
+        alert('Registration failed: ' + error.message)
+      }
+    },
+    async showUserInfo() {
+      try {
+        const response = await axios.get(`/api/v1/users/${this.userId}`)
+        this.userInfo = response.data.user
+      } catch (error) {
+        alert('Failed to get user info: ' + error.message)
+      }
+    }
+  }
 }
 </script>
 
-<style scoped>
-/* Add your styles here */
+<style>
+
 </style>
