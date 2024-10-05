9cc62414dedaf0bbb98ab485cee5c0a77a36785d
Danis Music <danism@student.chalmers.se>
2024-10-05 14:17:36 +0200

    Issue#23 Add new practice file for practicing on cards in a deck

---
+++ client/src/views/Practice.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <h1>Practice Mode</h1>
+    </div>
+
+</template>
+
+<script>
+import { Api } from '@/Api.js'
+
+export default {
+  props: {
+    deckId: {
+      type: String,
+      required: true
+    },
+    data() {
+      return {
+        practiceCards: [],
+        hardCards: [],
+        showExplanation: false,
+        currentCard: null
+      }
+    },
+    mounted() {
+      this.getPracticeCards()
+    },
+    methods: {
+      async getPracticeCards() {
+        const userId = localStorage.getItem('userId')
+        const response = await Api.get(`/v1/users/${userId}/decks/${this.deckId}/cards`)
+        this.practiceCards = response.data.cards
+        console.log(response.data.cards)
+      }
+    }
+  }
+}
+</script>
