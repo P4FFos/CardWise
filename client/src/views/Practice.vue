acce3796c15bb987945deb092de4f5d8af304d92
Danis Music <danism@student.chalmers.se>
2024-10-05 14:51:02 +0200

    Issue#23 Implement methods and template for practice mode in specific deck

--- client/src/views/Practice.vue
+++ client/src/views/Practice.vue
@@ -1,6 +1,18 @@
 <template>
     <div>
         <h1>Practice Mode</h1>
+        <div v-if="currentCard">
+            <p v-if="!showExplanation"> {{ currentCard.content }}</p>
+            <p v-else>{{ currentCard.explanation }}</p>
+            <button v-if="!showExplanation" @click="showExplanation = true">Explanation</button>
+            <div v-else>
+                <button @click="handleCard('easy')">Easy</button>
+                <button @click="handleCard('hard')">Hard</button>
+            </div>
+        </div>
+        <div v-else>
+            <p>All cards completed for this practice round</p>
+        </div>
     </div>
 
 </template>
@@ -13,26 +25,53 @@ export default {
     deckId: {
       type: String,
       required: true
-    },
-    data() {
-      return {
-        practiceCards: [],
-        hardCards: [],
-        showExplanation: false,
-        currentCard: null
+    }
+  },
+  data() {
+    return {
+      practiceCards: [],
+      hardCards: [],
+      showExplanation: false,
+      currentCard: null
+    }
+  },
+  mounted() {
+    this.getPracticeCards()
+  },
+  methods: {
+    nextCard() {
+      if (this.practiceCards.length > 0) {
+        this.currentCard = this.practiceCards.shift()
+        this.showExplanation = false
+      } else if (this.hardCards.length > 0) {
+        this.practiceCards = this.hardCards
+        this.hardCards = []
+        this.nextCard()
+      } else {
+        this.currentCard = null
       }
     },
-    mounted() {
-      this.getPracticeCards()
-    },
-    methods: {
-      async getPracticeCards() {
-        const userId = localStorage.getItem('userId')
+    async getPracticeCards() {
+      const userId = localStorage.getItem('userId')
+      try {
         const response = await Api.get(`/v1/users/${userId}/decks/${this.deckId}/cards`)
-        this.practiceCards = response.data.cards
-        console.log(response.data.cards)
+        this.practiceCards = response.data.deck.cards
+
+        console.log(response.data.deck.cards)
+        this.nextCard()
+      } catch (error) {
+        console.error('Error fetching cards from deck: ', error)
       }
+    },
+    handleCard(difficulty) {
+      if (difficulty === 'hard') {
+        this.hardCards.push(this.currentCard)
+        console.log('Current card has been labeled as hard: ', this.currentCard)
+      }
+      console.log('Current card has been labeled as easy: ', this.currentCard)
+      this.nextCard()
     }
   }
 }
+
 </script>
